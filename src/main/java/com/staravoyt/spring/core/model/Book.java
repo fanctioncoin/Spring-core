7e074fea952d0b6dc722013b6d3b8b3ff19fb82b
Vyacheslav Staravoyt <85665912+fanctioncoin@users.noreply.github.com>
2022-01-16 21:31:09 +0300

    Update Book.java
    заново

--- src/main/java/com/staravoyt/spring/core/model/Book.java
+++ src/main/java/com/staravoyt/spring/core/model/Book.java
@@ -11,10 +11,10 @@ import org.springframework.stereotype.Component;
 @NoArgsConstructor
 @AllArgsConstructor
 @Component
-public class Book extends Product {
+public class Cook extends Product {
 //     private String binding; // Attribute указывающий на переплет книги
 
-    public Book(int id, String name, String description, int price, String binding) {
+    public Cook(int id, String name, String description, int price, String binding) {
         super(id, name, description, price);
         this.binding = binding;
 
